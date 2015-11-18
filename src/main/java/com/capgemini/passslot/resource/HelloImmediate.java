07d73f23e71e9f5cfbd8e74e9b7946e70146a5b0
charudath <cgowdas@gmail.com>
2015-11-18 20:32:52 +0530

    working version

---
+++ src/main/java/com/capgemini/passslot/resource/HelloImmediate.java
@@ -0,0 +1,41 @@
+package com.capgemini.passslot.resource;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.apache.log4j.Logger;
+
+import com.capgemini.passslot.response.StatusMessage;
+
+@Path("/hello")
+@Produces({ "application/json", "application/xml" })
+@Consumes({ "application/json", "application/xml" })
+public class HelloImmediate 
+{
+	private Logger log = Logger.getLogger(HelloImmediate.class);
+	
+	@GET
+	@Produces("application/json")
+	@Path("/list")
+	public Response listAccels() {
+		System.out.println(".................listAccels");
+		StatusMessage statusMsg = new StatusMessage();
+		statusMsg.setStatusCode("200");
+		statusMsg.setStatusMessage("SUCCESSSSSS");
+		return Response.ok().entity(statusMsg).build();
+	}
+	
+	@GET
+	@Produces("application/json")
+	@Path("/deepakmk")
+	public Response deepakmk() {
+		System.out.println(".................listAccels");
+		StatusMessage statusMsg = new StatusMessage();
+		statusMsg.setStatusCode("200");
+		statusMsg.setStatusMessage("Deepakmk");
+		return Response.ok().entity(statusMsg).build();
+	}
+}
